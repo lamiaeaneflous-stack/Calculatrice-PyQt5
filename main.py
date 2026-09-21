77f47802a3634eeb72b59f2fc0476380b4c794c2
lam <lamiaeaneflous@gmail.com>
2026-09-21 19:04:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from calculatrice import Calculatrice
-
-
 app = QApplication(sys.argv)
-
 fenetre = Calculatrice()
 fenetre.show()
-
 sys.exit(app.exec_())
